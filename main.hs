fd58d777bf9e0cf01c315393e38ad06a3558cc87
connor-r78 <connorrakov@gmail.com>
2025-12-30 20:59:38 +0530

    Invalid usage message

--- main.hs
+++ main.hs
@@ -1,9 +1,23 @@
-module Main (main) where
+module Main ( main ) where
 
-import System.Environment (getArgs)
+import System.Environment
+import System.IO
 
-main :: IO ()
+usage = do
+  progName <- getProgName
+  putStrLn ("Usage: " ++ progName ++ " <input file>")
+
 main = do
   args <- getArgs
   case args of
-    [file] -> putStrLn ("Input: " ++ file)
+    [file] -> compile file
+    _ -> usage
+
+compile :: FilePath -> IO ()
+compile file =
+  withFile file ReadMode $ \handle -> do
+    slm <- getLines handle
+    sequence_ $ map putStrLn slm
+
+getLines :: Handle -> IO [String]
+getLines h = hGetContents h >>= return . lines
